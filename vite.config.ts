e4141bf2633ebfa775c6248d66b06d469597ed40
esdmr <esdmr0@gmail.com>
2023-06-23 15:54:53 +0330

    Use vite

---
+++ vite.config.ts
@@ -0,0 +1,47 @@
+import {defineConfig} from 'vite';
+import MagicString from 'magic-string';
+import {simple} from 'acorn-walk';
+
+export default defineConfig({
+	cacheDir: 'node_modules/.cache/vite',
+	build: {
+		target: ['firefox103', 'chrome104'],
+		outDir: 'build',
+		rollupOptions: {
+			input: ['index.html'],
+		},
+	},
+
+	plugins: [
+		{
+			name: 'bigint to number',
+			enforce: 'post',
+			transform(code, id) {
+				if (id.endsWith('.css') || id.includes('node_modules')) return null;
+
+				const ast = this.parse(code);
+				const s = new MagicString(code);
+
+				simple(ast, {
+					/* eslint-disable @typescript-eslint/naming-convention */
+					Literal(n: acorn.Node) {
+						if ('bigint' in n) {
+							s.update(n.start, n.end, n.bigint as string);
+						}
+					},
+					Identifier(n: acorn.Node) {
+						if ((n as any).name === 'BigInt') {
+							s.update(n.start, n.end, 'Number');
+						}
+					},
+					/* eslint-enable @typescript-eslint/naming-convention */
+				});
+
+				return {
+					code: s.toString(),
+					map: s.generateMap(),
+				};
+			},
+		},
+	],
+});
