000181ef5311785ba66d697701d7d78833eb602a
esdmr <esdmr0@gmail.com>
2023-06-26 11:34:28 +0330

    Just use a numeric value for enums

--- lib/tile-type.ts
+++ lib/tile-type.ts
@@ -1,14 +1,14 @@
-export const empty = import.meta.env.DEV ? 'empty' : 0;
-export const io = import.meta.env.DEV ? 'io' : 1;
-export const negate = import.meta.env.DEV ? 'negate' : 2;
-export const conjoinN = import.meta.env.DEV ? 'conjoinN' : 3;
-export const conjoinS = import.meta.env.DEV ? 'conjoinS' : 4;
-export const conjoinE = import.meta.env.DEV ? 'conjoinE' : 5;
-export const conjoinW = import.meta.env.DEV ? 'conjoinW' : 6;
-export const disjoinN = import.meta.env.DEV ? 'disjoinN' : 7;
-export const disjoinS = import.meta.env.DEV ? 'disjoinS' : 8;
-export const disjoinE = import.meta.env.DEV ? 'disjoinE' : 9;
-export const disjoinW = import.meta.env.DEV ? 'disjoinW' : 10;
+export const empty = 0;
+export const io = 1;
+export const negate = 2;
+export const conjoinN = 10;
+export const conjoinS = 11;
+export const conjoinE = 12;
+export const conjoinW = 13;
+export const disjoinN = 20;
+export const disjoinS = 21;
+export const disjoinE = 22;
+export const disjoinW = 23;
 
 export type QuadTreeTileType =
 	| typeof empty
