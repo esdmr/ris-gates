f17e532320403bf0479c823fe37d3cb89dffb0f2
esdmr <esdmr0@gmail.com>
2023-06-26 14:06:47 +0330

    Use one eslint-disable for canvas

--- src/input/canvas.ts
+++ src/input/canvas.ts
@@ -1,9 +1,8 @@
+/* eslint-disable @internal/explained-casts */
 import {assert} from '../lib/assert.js';
 
-// Cast safety: We assert afterwards.
 export const canvas = document.querySelector<HTMLCanvasElement>('#canvas')!;
 assert(canvas);
 
-// Cast safety: We assert afterwards.
 export const context = canvas.getContext('2d')!;
 assert(context);
