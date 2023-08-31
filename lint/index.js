135819b168c1f0cc3967020ee4962ad0298547bc
esdmr <esdmr0@gmail.com>
2023-08-31 14:00:25 +0330

    New lint rule: type-only imports consistency

--- lint/index.js
+++ lint/index.js
@@ -2,5 +2,6 @@ module.exports = {
 	rules: {
 		'explained-casts': require('./rules/explained-casts.js'),
 		'no-object-literals': require('./rules/no-object-literals.js'),
+		'type-only-imports': require('./rules/type-only-imports.js'),
 	},
 };
