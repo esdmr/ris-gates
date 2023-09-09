3dc4479e34f0af0507fba813dbd8cffea3a51d3d
esdmr <esdmr0@gmail.com>
2023-09-09 23:20:44 +0330

    Replace default tree with schematic

--- src/component/tree.ts
+++ src/component/tree.ts
@@ -1,30 +1,42 @@
 import {defaultScale, maximumScale, minimumScale} from '../lib/constants.js';
 import {FloatingBigInt} from '../lib/floating-bigint.js';
 import {Point} from '../lib/point.js';
-import * as searchMode from '../lib/search-mode.js';
+import {Schematic} from '../lib/schematic.js';
 import * as tileType from '../lib/tile-type.js';
 import {QuadTree} from '../lib/tree.js';
 
-export let tree = /* @__PURE__ */ createDefaultTree();
+const defaultTree = new Schematic(5, 5, [
+	tileType.empty,
+	tileType.empty,
+	tileType.io,
+	tileType.empty,
+	tileType.empty,
 
-function createDefaultTree() {
-	const tree = new QuadTree();
+	tileType.empty,
+	tileType.conjoinS,
+	tileType.disjoinS,
+	tileType.empty,
+	tileType.empty,
 
-	// SR Nor Latch
-	tree.getTileData(new Point(0n, 2n), searchMode.make).type = tileType.io;
-	tree.getTileData(new Point(1n, 1n), searchMode.make).type = tileType.conjoinS;
-	tree.getTileData(new Point(1n, 2n), searchMode.make).type = tileType.conjoinE;
-	tree.getTileData(new Point(2n, 0n), searchMode.make).type = tileType.io;
-	tree.getTileData(new Point(2n, 1n), searchMode.make).type = tileType.disjoinS;
-	tree.getTileData(new Point(2n, 2n), searchMode.make).type = tileType.negate;
-	tree.getTileData(new Point(2n, 3n), searchMode.make).type = tileType.conjoinN;
-	tree.getTileData(new Point(2n, 4n), searchMode.make).type = tileType.io;
-	tree.getTileData(new Point(3n, 2n), searchMode.make).type = tileType.disjoinW;
-	tree.getTileData(new Point(3n, 3n), searchMode.make).type = tileType.conjoinW;
-	tree.getTileData(new Point(4n, 2n), searchMode.make).type = tileType.io;
+	tileType.io,
+	tileType.conjoinE,
+	tileType.negate,
+	tileType.disjoinW,
+	tileType.io,
 
-	return tree;
-}
+	tileType.empty,
+	tileType.empty,
+	tileType.conjoinN,
+	tileType.conjoinW,
+	tileType.empty,
+
+	tileType.empty,
+	tileType.empty,
+	tileType.io,
+]);
+
+export let tree = new QuadTree();
+tree.putSchematic(defaultTree, new Point(1n, 1n));
 
 export function replaceTree(newTree: QuadTree) {
 	tree = newTree;
